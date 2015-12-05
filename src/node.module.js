53beeecbc7871b57088db49ca9e863d293de88a6
Michal Rokita <michal.rokita17@gmail.com>
2015-12-05 23:12:52 +0100

    New methods
    .text, .html, .get

--- src/node.module.js
+++ src/node.module.js
@@ -6,58 +6,128 @@ var coreJS = coreJS || {};
     coreJS.node = function (query) {
 
         var t = this;
-        var _ = {};
-        this._lastQ = this._q || query;
-        this._q = query;
-        this._document = this._document || document;
-        this._selectors = /^(#?[\w\-]+|\.[\w-.]+)$/;
-        this._domCache = this._domCache || {};
+        var _ = _ || {};
+        t.lastQ = t.lastQ || '';
+        t.q = query || t.q;
+        t.domCache = t.domCache || {};
 
-        this._splitClassesByDots = function (classes) {
+        /*/
+         * PRIVATE VARIABLES & METHODS
+        /*/
+
+        _.document = document;
+        _.selectors = /^(#?[\w\-]+|\.[\w-.]+)$/;
+
+        _.splitClassesByDots = function (classes) {
             return classes.replace(/\./g, ' ');
         };
 
-        this._queryAnalyzer = function () {
-            if (t._selectors.test(t._q)) {
-                switch (t._q.charAt(0)) {
+        _.queryAnalyzer = function (selector) {
+            if (_.selectors.test(selector)) {
+                switch (selector.charAt(0)) {
                 case '#':
-                    t._domCache[t._q] = t._document.getElementById(t._q.substr(1));
+                    t.domCache[selector] = _.document.getElementById(selector.substr(1));
                     break;
                 case '.':
-                    t._domCache[t._q] = t._document.getElementsByClassName(t._splitClassesByDots(t._q));
+                    t.domCache[selector] = _.document.getElementsByClassName(_.splitClassesByDots(selector));
                     break;
                 default:
-                    t._domCache[t._q] = t._document.getElementsByTagName(t._q);
+                    t.domCache[selector] = _.document.getElementsByTagName(selector);
                 }
             } else {
-                t._domCache[t._q] = t._document.querySelectorAll(t._q);
+                t.domCache[selector] = _.document.querySelectorAll(selector);
             }
-        };
-        
-        this._find = function () {
-            t._domCache[t._q] === undefined ? t._queryAnalyzer() : false; 
+
+            return t.domCache[selector];
         };
 
-        query ? this._find() : false;
-        
+        _.escapeHtml = function (string) {
+            return string
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#39;");
+        };
+
+        _.find = function (selector) {
+            var find = t.domCache[selector] === undefined ? _.queryAnalyzer(selector) : t.domCache[selector];
+            t.lastQ = find;
+            return find;
+        };
+
+        _.each = function (callback, params) {
+            Object.keys(t.lastQ).forEach(function (key) {
+                callback(t.lastQ[key], params);
+            });
+        };
+
+        query ? _.find(t.q) : false;
+
+        /*/
+         * PUBLIC VARIABLES & METHODS
+        /*/
+
+        t.isAnObject = function (element) {
+            if (typeof element === 'object' && element.length === undefined) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        t.isAString = function (element) {
+            if (typeof element === 'string') {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        t.isAnArray = function (element) {
+            if (typeof element === 'object' && element.length !== undefined) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        t.html = function (html) {
+            if (t.isAnArray(t.lastQ)) {
+                _.each(function (node, html) {
+                    node.innerHTML = html;
+                }, html);
+            } else {
+                t.lastQ.innerHTML = html;
+            }
+            
+            return t;
+        };
+
+        t.text = function (text) {
+            text = _.escapeHtml(text);
+            if (t.isAnArray(t.lastQ)) {
+                _.each(function (node, text) {
+                    node.innerHTML = text;
+                }, text);
+            } else {
+                t.lastQ.innerHTML = text;
+            }
+            
+            return t;
+        };
+
+        t.get = function () {
+            return t.lastQ;
+        };
+
         return this;
     };
 
 
 }(window, document, window.MutationObserver));
 
-coreJS.node('#parent');
-coreJS.node('.child');
+coreJS.node('#parent').html('#parent');
+coreJS.node('.child').text('.child').html('script');
 
 window.console.log(coreJS.node());
-
-
-
-
-
-
-
-
-
-
-
